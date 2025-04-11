fdb641414190dc613e8a7210bfbd24ceac766a8a
zhangsongbo <zhangp365@gmail.com>
2025-04-11 15:54:17 +0800

    fix a class bug.

--- myPredict.py
+++ myPredict.py
@@ -3,13 +3,57 @@ from typing import List
 
 from cog import BasePredictor, Input, Path 
 from predict import Predictor, Inputs
-from bfl_predictor import BflReduxPredictor
-from predict import FLUX_DEV
+from bfl_predictor import BflReduxPredictor, BflBf16Predictor
+from predict import FLUX_DEV, LoadedModels, WeightsDownloadCache
+from flux.sampling import (
+    prepare_redux,
+)
+from flux.util import (
+    load_redux,
+)
+
+class BflReduxLoraPredictor(BflBf16Predictor):
+    """
+    Works for dev and schnell.
+    To use, pass path to redux image into predict as a prepare_kwargs - e.g.:
+        redux_predictor.predict(..., prepare_kwargs={"redux_img_path": redux_image}
+    """
+
+    def __init__(
+        self,
+        flow_model_name: str,
+        loaded_models: LoadedModels | None = None,
+        device: str = "cuda",
+        offload: bool = False,
+        weights_download_cache: WeightsDownloadCache | None = None,
+        restore_lora_from_cloned_weights: bool = False,
+    ):
+        super().__init__(
+            flow_model_name,
+            loaded_models,
+            device=device,
+            offload=offload,
+            weights_download_cache=weights_download_cache,
+            restore_lora_from_cloned_weights=restore_lora_from_cloned_weights,
+        )
+        self.redux_image_encoder = load_redux(device="cuda")
+
+    def prepare(self, x, prompt, redux_img_path=None):
+        """Overrides prepare in order to properly preprocess redux image"""
+        return prepare_redux(
+            self.t5,
+            self.clip,
+            x,
+            prompt=prompt,
+            encoder=self.redux_image_encoder,
+            img_cond_path=redux_img_path,
+        )
+
 
 class DevReduxLoraPredictor(Predictor):
     def setup(self):
         self.base_setup()
-        self.model = BflReduxPredictor(FLUX_DEV, 
+        self.model = BflReduxLoraPredictor(FLUX_DEV, 
                                        restore_lora_from_cloned_weights=True,
                                        offload=self.should_offload())
         
