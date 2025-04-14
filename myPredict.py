63088400c9b78faa0d2838594fdc22e804549557
zhangsongbo <zhangp365@gmail.com>
2025-04-14 17:37:53 +0800

    fix a getting device bug.

--- myPredict.py
+++ myPredict.py
@@ -85,7 +85,8 @@ class DevReduxLoraPredictor(Predictor):
         lora_weights: str = Inputs.lora_weights,
         lora_scale: float = Inputs.lora_scale,
     ) -> List[Path]:
-        self.model.handle_loras(lora_weights, lora_scale, self.model.model.device)
+        device = next(self.model.model.parameters()).device
+        self.model.handle_loras(lora_weights, lora_scale, device=device)
 
         width, height = self.size_from_aspect_megapixels(aspect_ratio, megapixels)
         imgs, np_imgs = self.model.predict(
